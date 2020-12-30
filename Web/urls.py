036936dd38eb00452b38305ddcfdcbe21cffdb8c
Keremhan <keremhanmd@outlook.com>
2020-12-30 13:40:37 +0300

    Files
    Project files

---
+++ Web/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('city/', views.city, name="city"),
+    path('comments/', views.comments, name="comment"),
+    path('list/', views.list, name="list"),
+    path('mcomm/', views.makecomm, name="mc"),
+    path('place/', views.place, name="place"),
+    path('signin/', views.signin, name="si"),
+    path('signup', views.signup, name="su"),
+
+]
---
+++ Web/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+
+def home(request):
+    return render(request, 'Pages/Home.html')
+
+
+def city(request):
+    return render(request, 'Pages/City.html')
+
+
+def comments(request):
+    return render(request, 'Pages/Comments.html')
+
+
+def list(request):
+    return render(request, 'Pages/List.html')
+
+
+def makecomm(request):
+    return render(request, 'Pages/makecomm.html')
+
+
+def place(request):
+    return render(request, 'Pages/Place.html')
+
+
+def signin(request):
+    return render(request, 'Pages/Signin.html')
+
+
+def signup(request):
+    return render(request, 'Pages/Signup.html')
