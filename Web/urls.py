5f7ab77de5da2cf5e691845ed6a2cf04dd53cbcd
ErayGurdereoglu <76774953+ErayGurdereoglu@users.noreply.github.com>
2021-01-14 13:35:45 +0300

    last

--- Web/urls.py
+++ Web/urls.py
@@ -6,10 +6,9 @@ urlpatterns = [
     path('city/', views.city, name="city"),
     path('comments/', views.comments, name="comment"),
     path('list/', views.list, name="list"),
-    path('mcomm/', views.makecomm, name="mc"),
+    path('makecomm/', views.makecomm, name="mc"),
     path('place/', views.place, name="place"),
     path('signin/', views.signin, name="si"),
     path('signup', views.signup, name="su"),
     path('howtogo/', views.howToGo, name="howTo"),
-
 ]
