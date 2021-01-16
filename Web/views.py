3ae31ba834b7e50107a9545d73e3c2f154a3bd2a
Keremhan <keremhanmd@outlook.com>
2021-01-16 14:45:16 +0300

    haha pain go brrrrrrrrrrrrrrrrrrrrr

--- Web/views.py
+++ Web/views.py
@@ -4,6 +4,7 @@ from .models import *
 from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import CreateUserForm
 from django.contrib import messages
@@ -20,11 +21,11 @@ def city(request):
 def comments(request):
     return render(request, 'Pages/Comments.html')
 
-
+@login_required(login_url='login')
 def list(request):
     return render(request, 'Pages/List.html')
 
-
+@login_required(login_url='login')
 def makecomm(request):
     return render(request, 'Pages/makecomm.html')
 
@@ -68,17 +69,22 @@ def signup(request):
 def howToGo(request):
     return render(request,'Pages/howtogo.html')
 
+@login_required(login_url='login')
 def homel(request):
     return render(request, 'Pages/Homel.html')
 
+@login_required(login_url='login')
 def cityl(request):
     return render(request, 'Pages/Cityl.html')
 
+@login_required(login_url='login')
 def commentsl(request):
     return render(request, 'Pages/Commentsl.html')
 
+@login_required(login_url='login')
 def placel(request):
     return render(request, 'Pages/Placel.html')
 
+@login_required(login_url='login')
 def howToGol(request):
     return render(request,'Pages/howtogol.html')
